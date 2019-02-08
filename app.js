e685d37fbad2ddd93a64e7c79d165829396454d2
Loya0511 <loya.akshat@gmail.com>
2019-02-08 21:17:35 +0530

    Bricks

--- app.js
+++ app.js
@@ -1,3 +1,33 @@
+n = 13, brickSize = undefined;
 
 canvas = document.getElementById("Canvas");
+pen = canvas.getContext("2d");
+brickSize = canvas.width/n;
+
+position = function(a,b){
+	x=a;
+	y=b;
+}
+
+brick = {
+	x = position.x, y = position.y;
+
+	draw: function(){
+		pen.beginPath();
+		pen.rect(x, y, brickSize, brickSize);
+		pen.fll();
+		pen.closePath();
+	}
+}
+
+layout = {
+	for (var i = 0; i < canvas.height; i=i+2*brickSize) {
+		for(var j = 0; j < canvas.width; j=j+brickSize){
+			position(i,j);
+			brick.draw();
+		}
+
+	}
+	
+}
 
