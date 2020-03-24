49f529d03240e3857114b5e9e2fed67fdc3a5653
drpeabody <abhiagar97@gmail.com>
2020-03-24 18:15:01 +0530

    Added Attack and Pass

---
+++ MRisk/attack.js
@@ -0,0 +1,16 @@
+
+
+// Returns Message to be displayed don HUD
+
+resolveAttack = (attacker, victim) => {
+	// Get all information
+
+	// Make calculations attack
+
+	// Resolve Attack
+
+	// Change Zone properties for victim and attacker
+
+	// Return Message
+
+}
